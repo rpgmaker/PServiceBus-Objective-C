343c05de3655f802a15afe2b5152b7658c12f5b8
rpgmaker <olamide.bakre@gmail.com>
2013-02-18 23:35:19 -0500

    added test code for blocks

--- src/test.m
+++ src/test.m
@@ -15,6 +15,37 @@
 
 @end
 
+struct BlockDescriptor {
+    unsigned long reserved;
+    unsigned long size;
+    void *rest[1];
+};
+
+struct Block {
+    void *isa;
+    int flags;
+    int reserved;
+    void *invoke;
+    struct BlockDescriptor *descriptor;
+};
+
+static const char *BlockSig(id blockObj)
+{
+    struct Block *block = (void *)blockObj;
+    struct BlockDescriptor *descriptor = block->descriptor;
+
+    int copyDisposeFlag = 1 << 25;
+    int signatureFlag = 1 << 30;
+
+    assert(block->flags & signatureFlag);
+
+    int index = 0;
+    if(block->flags & copyDisposeFlag)
+        index += 2;
+
+    return descriptor->rest[index];
+}
+
 int main (int argc, const char * argv[])
 {
 	@autoreleasepool {
@@ -38,6 +69,16 @@ int main (int argc, const char * argv[])
 		NSLog(@"%@", json);
 		TestObject *obj2 = (TestObject *)[PSBJSONParser jsonToObject: json clazz: [TestObject class]];
 		NSLog(@"%@", obj2.setting);
+
+
+        void (^block)(TestObject *) = ^(TestObject *o) {
+            NSLog(@"%@ %@", NSStringFromClass([o class]), o);
+
+        };
+        NSLog(@"signature %s", BlockSig(block));
+        block(obj2);
+
+		//NSLog(@"%@", [NSString stringWithCString:@encode(TestObject) encoding:NSASCIIStringEncoding]);
 	}
 
 	return 0;
